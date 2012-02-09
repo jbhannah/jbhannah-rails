a43c7bcbbdab058b064fe010d17fefdc0e6f451f
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-09 07:54:09 -0700

    Generated new Rails site
    `rails new jbhannah -T`
    `rvm --rvmrc 1.9.3@jbhannah`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jbhannah::Application.config.secret_token = '424adcb7821c7d86be82cef747c98a0bf0b61d3f3ba73d5aeb3ed21951a5e3d15fa094d62760b9316a0e2a0e91485c4953a0d915dfd4bb9a2a2d517f1f9c0438'
