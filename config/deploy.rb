9e9282701b8421b66c29cb2cc8049f0a7962b497
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-11 11:21:59 -0700

    Clean up deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 require "bundler/capistrano"
 
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, '1.9.3@jbhannah'        # Or whatever env you want it to run in.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_ruby_string, '1.9.3@jbhannah'
 
 set :user, "jbhannah"
 set :domain, "lugia.jbhannah.net"
@@ -13,23 +13,9 @@ set :application, "jbhannah"
 set :repository,  "git://github.com/jbhannah/jbhannah.git"
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, domain                          # Your HTTP server, Apache/etc
-role :app, domain                          # This may be the same as your `Web` server
-role :db,  domain, :primary => true # This is where Rails migrations will run
+server domain, :app, :web, :db, :primary => true
 
-# if you"re still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,"tmp","restart.txt")}"
-#   end
-# end
 namespace :deploy do
   task :chown do
     run "#{try_sudo} chown -R #{user}:#{user} #{File.join(deploy_to)}"
@@ -50,7 +36,7 @@ end
 
 namespace :nginx do
   task :symlink do
-    run "cd /etc/nginx/sites-enabled; #{try_sudo} rm -f jbhannah; #{try_sudo} ln -s #{File.join(current_path, "config", "nginx.conf")} #{application}"
+    run "cd /etc/nginx/sites-enabled; #{try_sudo} rm -f #{application}; #{try_sudo} ln -s #{File.join(current_path, "config", "nginx.conf")} #{application}"
   end
 
   task :reload do
@@ -59,6 +45,8 @@ namespace :nginx do
 end
 
 after 'deploy:setup', 'deploy:chown'
+
 after 'deploy:start', 'nginx:symlink'
 after 'deploy:restart', 'nginx:symlink'
+
 after 'nginx:symlink', 'nginx:reload'
