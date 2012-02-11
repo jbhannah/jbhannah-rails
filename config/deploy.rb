328f84e816dfa6f8cfedf0016670ebd57ca8c8fc
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-10 23:44:57 -0700

    RVM Capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,9 @@
 require "bundler/capistrano"
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.3@jbhannah'        # Or whatever env you want it to run in.
+
 set :user, "jbhannah"
 set :domain, "lugia.jbhannah.net"
 
