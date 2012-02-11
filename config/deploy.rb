7a5635199b136df33842be5a883b754d9f4905a2
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-10 23:21:04 -0700

    Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+require "bundler/capistrano"
+set :user, "username"
+set :domain, "lugia.jbhannah.net"
+
+set :application, "jbhannah"
+set :repository,  "git://github.com/jbhannah/jbhannah.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+# if you"re still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,"tmp","restart.txt")}"
+#   end
+# end
