1abeea577eea8eb986b544e50907b09d4c4207e8
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-12 16:57:20 -0700

    Short bio on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    d  = Time.now
+    bd = Time.new(1989, 3, 20)
+
+    @age = d.year - bd.year
+    @age = @age - 1 if (
+       bd.month >  d.month or 
+      (bd.month >= d.month and bd.day > d.day)
+    )
   end
 
   def redirect
