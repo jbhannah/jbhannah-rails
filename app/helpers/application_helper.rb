e39237953c13724a6cdcce183eeb05fd292b98db
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-12 17:03:48 -0700

    Extract age function to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def age
+    d  = Date.today
+    bd = Date.new(Settings.birthday.year, Settings.birthday.month, Settings.birthday.day)
+
+    a = d.year - bd.year
+    a = a - 1 if (
+       bd.month >  d.month or
+      (bd.month >= d.month and bd.day > d.day)
+    )
+    a
+  end
 end
