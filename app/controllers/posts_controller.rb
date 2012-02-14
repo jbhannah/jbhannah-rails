85767ebc1339828df12a7ed3acd2783ac478ceab
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-14 10:07:16 -0700

    will_paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   load_resource
 
   def index
+    @posts = @posts.page(params[:page])
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :title, :user_id, :body
+
+  self.per_page = 10
 end
