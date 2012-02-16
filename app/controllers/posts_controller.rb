38e17c95d35d825cc9713ef86dbc2925aa4f1ae4
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-15 18:55:26 -0700

    Blog archive page headers and titles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,10 @@ class PostsController < ApplicationController
 
   def index
     if params[:year]
-      t = Time.new(params[:year], params[:month], params[:day])
-      @posts = @posts.where(published_at: t..(t + 1.year))
-      @posts = @posts.where(published_at: t..(t + 1.month)) if params[:month]
-      @posts = @posts.where(published_at: t..(t + 1.day))   if params[:day]
+      @date = Time.new(params[:year], params[:month], params[:day])
+      @posts = @posts.where(published_at: @date..(@date + 1.year))
+      @posts = @posts.where(published_at: @date..(@date + 1.month)) if params[:month]
+      @posts = @posts.where(published_at: @date..(@date + 1.day))   if params[:day]
     end
 
     @posts = @posts.page(params[:page])
