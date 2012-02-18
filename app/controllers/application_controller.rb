f9423467f218dca28639fa9a5de5e63c30940037
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-17 19:14:14 -0700

    Blog post cache sweeper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  cache_sweeper :post_sweeper
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path
---
+++ app/controllers/post_sweeper.rb
@@ -0,0 +1,18 @@
+class PostSweeper < ActionController::Caching::Sweeper
+  observe Post
+
+  def after_create(post)
+    expire_cache_for(post)
+  end
+
+  def after_update(post)
+    expire_cache_for(post)
+  end
+
+  def expire_cache_for(post)
+    expire_fragment "post_months" 
+    expire_fragment "latest_posts" 
+    expire_action controller: :posts, action: :index
+    expire_action controller: :posts, action: :show, id: post.slug
+  end
+end
