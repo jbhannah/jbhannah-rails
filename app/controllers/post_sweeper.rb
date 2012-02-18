fe92830361a893320ee094391278ef8b1c826e7c
Jesse B. Hannah <jesse@jbhannah.net>
2012-02-17 21:42:20 -0700

    Caching refactoring

--- app/controllers/post_sweeper.rb
+++ app/controllers/post_sweeper.rb
@@ -9,9 +9,11 @@ class PostSweeper < ActionController::Caching::Sweeper
     expire_cache_for(post)
   end
 
+  private
   def expire_cache_for(post)
     expire_fragment "post_months" 
     expire_fragment "latest_posts" 
     expire_fragment "post_#{post.id}"
+    expire_fragment "header_#{main_app.post_path(post)}"
   end
 end
